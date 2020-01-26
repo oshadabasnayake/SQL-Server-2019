8c2ecc33c3850c6ec95a155c66d03e8bef10d2a6
Oshada Basnayake <oshadasinhalaya@gmail.com>
2020-01-26 19:48:41 +0530

    Insert Values Property.sql
    Insert Values Property.sql

---
+++ test db 1/Insert Values Property.sql
@@ -0,0 +1,24 @@
+INSERT INTO Property
+	(
+
+	PropertyID,
+	LandlordID,
+	PropertyAddress,
+	NumOfTenants,
+	Rent,
+	Category
+	
+	)
+
+	VALUES
+
+	 (1000,10,'No3,Main Road,Colombo',2,8000,'Flat'),
+	 (1001,11,'No124,Udumulla,Battaramulla',4,10000,'T.H'),
+	 (1002,12,'No166,Naranwala,Gampaha',6,12000,'D.H'),
+	 (1003,13,'No35,Sea Road,Colombo',2,8500,'Flat'),
+	 (1004,14,'No35,Sea Road,Colombo',4,10000,'D.H'),
+	 (1005,15,'No3,Main Road,Colombo',6,12000,'T.H'),
+	 (1006,16,'No122,Udumulla,Battaramulla',3,6000,'Flat'),
+	 (1007,17,'No125,Westco,Nugegoda',4,13000,'T.H'),
+	 (1008,18,'No13,Yatamulla,Battaramulla',2,9850,'Flat'),
+	 (1009,19,'No11,Wathala,Colombo',4,9800,'T.H');
